617269788fe87e0e1837aa10ad8e995160fe10c1
Sergio Calbiño <serggio.c86@gmail.com>
2022-11-18 12:48:23 -0300

    Front y Back conectados

--- api/app.js
+++ api/app.js
@@ -1,14 +1,30 @@
 import "dotenv/config.js";
 import express from "express";
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
+import cors from 'cors'
 // import routerProducts from "./route/products.js";
 import config from "./config/config.js";
-
 import routerApi from "./route/index.js";
-const port = process.env.PORT;
+
+const port = process.env.PORT || 4000;
 const app = express();
 
 app.use(express.json());
+
+const dominiosPertmitidos = ['http://localhost:3000']
+
+const corsOptions = {
+  origin: function(origin, callback) {
+      if (dominiosPertmitidos.indexOf(origin) !== -1) {
+          //El origen del request esta permitido
+          callback(null, true)
+      } else {
+          callback(new Error('No permitido por CORS'))
+      }
+  }
+}
+
+app.use(cors(corsOptions))
 
 routerApi(app);
 
