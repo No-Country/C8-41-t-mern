f6fbe65c7ce4d55d24af892397bd2fcadbaaf702
Your Name <you@example.com>
2022-11-19 21:12:55 -0300

    Product Detail

--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -28,7 +28,7 @@ const Products = () => {
             return (
               <li key={index} className="products__list-item">
                 <div className="products__list-item__img">
-                  <img src={item.url} alt={item.name} />
+                  <img src={item.image} alt={item.name} />
                 </div>
                 <div className="products__list-item__content">
                   <h2>{item.name}</h2>
