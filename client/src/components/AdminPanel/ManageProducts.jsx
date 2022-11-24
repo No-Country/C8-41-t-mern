040e32835024a8165013f83edd57d13128c4e391
rafa2701 <leonr2701@gmail.com>
2022-11-24 00:19:36 -0400

    crear componente lista de productos

--- client/src/components/AdminPanel/ManageProducts.jsx
+++ client/src/components/AdminPanel/ManageProducts.jsx
@@ -1,45 +1,63 @@
-import React from 'react'
-import {Table, Container} from 'react-bootstrap';
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+//import Product from "../Products/Product";
+import {Table, Container} from "react-bootstrap";
 
 const ManageProducts = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    console.log("doc is " + import.meta.env.VITE_BACKEND_URL);
+    const traerProductos = () => {
+      const url = `${import.meta.env.VITE_BACKEND_URL}/api/products`;
+
+      axios
+        .get(url)
+        .then((res) => setProducts(res.data))
+        .catch((error) => console.log(error));
+    };
+    traerProductos();
+  }, []);
+  console.log("stock is "+products);
+  const checkStock = (stock) => {
+    // console.log(stock);
+    // let answer=0
+    // stock.stock.isNumber() ? answer=stock.stock : answer="Agotado"  
+    // return answer;
+    return "Agotado";
+  };
   return (
     <>
-    <Container>
-     <h1 className="text-center py-2">Inventario de Productos </h1> 
-    <Table striped>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </Table>
-    </Container>
-    
+      <Container>
+        <Table striped>
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>Producto</th>
+              <th>Stock</th>
+              <th>Vendidos</th>
+              <th>Opciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((item, index) => {
+              console.log(item);
+              return (
+                <tr>
+                  <td>{item.id}</td>
+                  <td>{item.name}</td>
+                  <td>{checkStock(item.stock)}</td>
+                  <td>{item.sold}</td>
+                  <td>{"X"}</td>
+                </tr>
+                // <Product item={item} key={index} />
+              );
+            })}
+          </tbody>
+        </Table>
+      </Container>
     </>
-    
-  )
-}
+  );
+};
 
-export default ManageProducts
+export default ManageProducts;
--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-
 import Product from "./Product";
 
 import "./Products.css";
