3c7bf0f16a681ae606114d2f91bae06fe2d502d2
rafa2701 <leonr2701@gmail.com>
2022-11-28 23:44:41 -0400

    añadidas notificaciones de eliminacion

--- client/src/components/AdminPanel/ManageProducts.jsx
+++ client/src/components/AdminPanel/ManageProducts.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 //import Product from "../Products/Product";
 import { Link } from "react-router-dom";
 import {Table, Container, Button} from "react-bootstrap";
+import Swal from "sweetalert2";
 
 const ManageProducts = () => {
   const [products, setProducts] = useState([]);
@@ -43,20 +44,45 @@ const ManageProducts = () => {
     // return answer;
     return "Agotado";
   };
-  const handleDelete = async (id, e) => {
+  const handleDelete = async (id, e, item) => {
     e.preventDefault();
     console.log("id is "+id);
     const target = `${import.meta.env.VITE_BACKEND_URL}/api/products/${id}`;
-    await axios
+    await Swal.fire({
+      position: "center",
+      icon: "warning",
+      title: `Desea Eliminar el producto ${item} ?`,
+      showDenyButton: true,
+      confirmButtonText: 'Si',
+      confirmButtonColor: 'green',
+      denyButtonText: `No`,
+      buttons: true,
+      //dangerMode: true,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        axios
         .delete(target)
         .then((res) => {
           console.log({res});
           console.log("item eliminado");
           //necesito forzar update
           // 
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: `El producto ${item} fue eliminado exitosamente`,
+            showConfirmButton: false,
+            timer: 2500,
+          });
           
         })
         .catch((error) => console.log(error));
+        //Swal.fire('Saved!', '', 'success')
+      } else if (result.isDenied) {
+        Swal.fire('El item no ha sido eliminado', '', 'info')
+      }});
+    
     
   };
   return (
@@ -74,7 +100,7 @@ const ManageProducts = () => {
           </thead>
           <tbody>
             {products.map((item, index) => {
-            console.log(item);
+            //console.log(item);
               return (
                 <tr key={item.id}>
                   <td>{item._id}</td>
@@ -86,7 +112,7 @@ const ManageProducts = () => {
                     <Button variant="success">Edit</Button>{' '}
                   {/* <Button variant="warning">Ed</Button>{' '} */}
                   <Button variant="danger" onClick={((e) => {
-                    handleDelete(item._id, e);
+                    handleDelete(item._id, e, item.name);
                   })}>Delete</Button>{' '}</td>
                 </tr>
                 // <Product item={item} key={index} />
