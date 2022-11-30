373fa8585712e2d63e6a22da029d3d8353c102fc
rafa2701 <leonr2701@gmail.com>
2022-11-29 20:02:53 -0400

    cambiada redireccion login a "micuenta"

--- client/src/hooks/useAuthStore.js
+++ client/src/hooks/useAuthStore.js
@@ -26,7 +26,7 @@ export const useAuthStore = () => {
             localStorage.setItem("user", data.user.name)
             sessionStorage.setItem("user",data.user.name)
             dispatch(onLogin({ name: data.user.name, email: data.user.email, uid: data.user.uid, state: data.user.state, phone: data.user.phone, street: data.user.street, zip: data.user.zip, isAdmin: data.user.isAdmin }))
-            navigate("/");
+            navigate("/micuenta");
         } catch (error) {
             let err = error.response.data.error?.map(err => err.msg)
             let err2 = error.response.data.msg
