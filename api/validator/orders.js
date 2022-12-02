ba6622fa39c08afe5ceb77f7f51a22d145dc3828
Hristo S. Oviedo <hristo.oviedohn@gmail.com>
2022-12-02 04:08:38 -0600

    perf(backend): Agregar validador para updateOrder y Modificar validador de createOrder

--- api/validator/orders.js
+++ api/validator/orders.js
@@ -6,13 +6,43 @@ const validateCreate = [
 
   check("shippingAddress.address").exists().bail().notEmpty().bail().trim(),
 
+  check("shippingAddress.city").optional().notEmpty().bail().trim(),
+
+  check("shippingAddress.zip").optional().notEmpty().bail().trim(),
+
   check("phone").exists().bail().notEmpty().bail().trim(),
 
-  check("userId").exists().bail().notEmpty().bail().trim(),
+  check("totalPrice").exists().notEmpty().bail().isDecimal(),
+
+  check("userId").exists().isMongoId().bail().notEmpty().bail().trim(),
 
   (req, res, next) => {
     validateResult(req, res, next);
   },
 ];
 
-export { validateCreate };
+const validateUpdate = [
+  check("orderItems").optional().isArray().notEmpty(),
+
+  check("shippingAddress.address").optional().notEmpty().bail().trim(),
+
+  check("shippingAddress.city").optional().notEmpty().bail().trim(),
+
+  check("shippingAddress.zip").optional().notEmpty().bail().trim(),
+
+  check("phone").optional().notEmpty().bail().trim(),
+
+  check("orderStatus").optional().notEmpty().bail().trim(),
+  
+  // check("deliveryDate").optional().notEmpty().bail().isDate(),
+
+  check("totalPrice").optional().notEmpty().bail().isDecimal(),
+
+  check("userId").optional().isMongoId().bail().notEmpty().bail().trim(),
+
+  (req, res, next) => {
+    validateResult(req, res, next);
+  },
+];
+
+export { validateCreate, validateUpdate };
