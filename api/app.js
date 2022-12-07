a8321cab5106038ae2dfc167b2e3bfb4198b05b6
Miguel Guerrero <91755159+Eiine@users.noreply.github.com>
2022-12-07 03:00:27 -0300

    Update app.js

--- api/app.js
+++ api/app.js
@@ -10,21 +10,21 @@ const app = express();
 
 app.use(express.json());
 
-//const dominiosPertmitidos = ["http://localhost:3001", "https://c8-41-t-mern.vercel.app/"];
+const dominiosPertmitidos = ["http://localhost:3000", "https://c8-41-t-mern.vercel.app/"];
 
- //const corsOptions = {
-   //origin: function (origin, callback) {
-     //if (dominiosPertmitidos.indexOf(origin) !== -1) {
+ const corsOptions = {
+   origin: function (origin, callback) {
+     if (dominiosPertmitidos.indexOf(origin) !== -1) {
        //El origen del request esta permitido
-       //callback(null, true);
-     //} else {
-      //callback(new Error("No permitido por CORS"));
-    //}
-  //},
- //};
+       callback(null, true);
+     } else {
+      callback(new Error("No permitido por CORS"));
+    }
+  },
+ };
 
 
- //app.use(cors(corsOptions));
+ app.use(cors(corsOptions));
 
 
 
