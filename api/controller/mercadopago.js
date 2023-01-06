a7408e9bd2302e3015e1abd7e4eb3c929ec3dc1f
Miguel Guerrero <yannokaiserfrom@gmail.com>
2023-01-06 02:39:16 -0300

    corrigiendo back para devolucion falta de items

--- api/controller/mercadopago.js
+++ api/controller/mercadopago.js
@@ -2,57 +2,60 @@ import axios from "axios";
 
 const compraMp = async (req, res) => {
   const order = req.body;
+  if (order.orderItems.length > 0) {
+    const url = "https://api.mercadopago.com/checkout/preferences";
+    const items = order.orderItems.map((item) => {
+      return {
+        title: item.name,
+        description: item.description,
+        currency_id: "ARS",
+        quantity: item.quantity,
+        unit_price: item.price,
+      };
 
-  const url = "https://api.mercadopago.com/checkout/preferences";
-  const items = order.orderItems.map((item) => {
-    return {
-      title: item.name,
-      description: item.description,
-      currency_id: "ARS",
-      quantity: item.quantity,
-      unit_price: item.price,
-    };
-
-    //notification_url: `${{}}`,
-  });
-
-  try {
-    const buyOrder = {
-      items: items,
-
-      // {
-      //   title: order.orderItems.name,
-      //       description: order.orderItems.description,
-      //       currency_id: "ARS",
-      //       quantity: order.orderItems.quantity,
-      //       unit_price: order.orderItems.price,
-      //       //notification_url: `${{}}`,
-      // },
-      payer: {
-        phone: { phone: order.phone },
-        identification: {},
-        address: order.shippingAddress,
-        email: order.email,
-      },
-      back_urls: {
-        success: "Todo salio bien",
-        pending: "Su pago esta pendiente",
-        failure: "El pago a fallado",
-      },
-      notification_url: "",
-    };
-
-    const result = await axios({
-      method: "post",
-      url: url,
-      headers: { Authorization: `Bearer ${process.env.ACCESS_TOKEN}` },
-      data: buyOrder,
+      //notification_url: `${{}}`,
     });
-    console.log(result.data.id);
-    // res.send(result.data.init_point); //ENLACE DE FORMATO DE PAGO
-    return res.send(result.data.id); //ID de preferencia del formato
-  } catch (error) {
-    console.log(error);
+
+    try {
+      const buyOrder = {
+        items: items,
+
+        // {
+        //   title: order.orderItems.name,
+        //       description: order.orderItems.description,
+        //       currency_id: "ARS",
+        //       quantity: order.orderItems.quantity,
+        //       unit_price: order.orderItems.price,
+        //       //notification_url: `${{}}`,
+        // },
+        payer: {
+          phone: { phone: order.phone },
+          identification: {},
+          address: order.shippingAddress,
+          email: order.email,
+        },
+        back_urls: {
+          success: "Todo salio bien",
+          pending: "Su pago esta pendiente",
+          failure: "El pago a fallado",
+        },
+        notification_url: "",
+      };
+
+      const result = await axios({
+        method: "post",
+        url: url,
+        headers: { Authorization: `Bearer ${process.env.ACCESS_TOKEN}` },
+        data: buyOrder,
+      });
+      console.log(result.data.id);
+      // res.send(result.data.init_point); //ENLACE DE FORMATO DE PAGO
+      return res.send(result.data.id); //ID de preferencia del formato
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    res.send("no hay items");
   }
 };
 
