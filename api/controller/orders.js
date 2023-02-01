5544c24e0c840a6e37915bffe453aecdf704d5d1
Miguel Guerrero <yannokaiserfrom@gmail.com>
2023-02-01 08:11:58 -0300

    logs en orders

--- api/controller/orders.js
+++ api/controller/orders.js
@@ -3,7 +3,7 @@ import { httpError } from "../helper/handleError.js";
 
 const getOrders = async (req, res) => {
   //Conseguir el estatus
-  console.log(req.query.status);
+
   try {
     const orders = await ordersModel.find();
     if (orders.length < 1) {
@@ -45,24 +45,22 @@ const getOneOrder = async (req, res) => {
 const createOrder = async (req, res) => {
   try {
     const body = req.body;
-    
-    const {street, phone, uid, totalPrice,cart } = body;
-    console.log( street, phone, uid, totalPrice);
+
+    const { street, phone, uid, totalPrice, cart } = body;
+    console.log(street, phone, uid, totalPrice);
     let total;
-    console.log(cart[0])
-   
+    console.log(cart[0]);
+
     // let suma = 0;
     // total = cart.map(element => {
     // console.log(element.price)
     //   suma += element.price
     //   // console.log(element.price)
     //    console.log(typeof(suma))
-      
+
     // });
-    
- 
+
     const newOrder = new ordersModel({
-      
       shippingAddress: street,
       phone,
       orderStatus: "En Proceso",
@@ -70,7 +68,7 @@ const createOrder = async (req, res) => {
       deliveryDate: new Date(), //TODO: Incrementar la fecha
       totalPrice: totalPrice,
       userId: uid,
-      orderItems:cart
+      orderItems: cart,
     });
     const saved = await newOrder.save();
     res.status(200).json(saved);
